da614bfe879c97afce22c46fad7cb1397c69699c
rod <rod@rod-XPS-8300>
2015-11-23 21:44:29 -0200

    clean up

--- webservices/src/main/java/com/rodes/web/HomeController.java
+++ webservices/src/main/java/com/rodes/web/HomeController.java
@@ -6,23 +6,16 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.rodes.domain.Movie;
-import com.rodes.service.MovieService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-
-	@Autowired
-	private MovieService movieService;
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
@@ -39,8 +32,6 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 
 		model.addAttribute("serverTime", formattedDate);
-
-		Iterable<Movie> movies = movieService.findAllMovies();
 
 		return "home";
 	}
